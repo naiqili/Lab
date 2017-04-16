458f007b74526ff133e60c65689c74c09d690d6a
naiqili <naiqili@github.com>
2017-04-16 13:58:09 +1000

    update

--- tf_nlu/test_target.sh
+++ tf_nlu/test_target.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --target='title' --modelfile='./model/best.data-00000-of-00001'
+CUDA_VISIBLE_DEVICES=0 python test.py --target='title' --modelfile='./model/best' --N=20
