ba308e9d8a513d26164eb5505fcc28369f21a9cd
naiqili <naiqili@github.com>
2017-04-18 16:55:19 +1000

    update

--- tf_nlu/train_all.sh
+++ tf_nlu/train_all.sh
@@ -1,6 +1,6 @@
-./train_title
-./train_whenst
-./train_whened
-./train_day
-./train_location
-./train_invitee
+./train_title.sh
+./train_whenst.sh
+./train_whened.sh
+./train_day.sh
+./train_location.sh
+./train_invitee.sh
