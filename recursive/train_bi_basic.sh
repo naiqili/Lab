136b3e8af59dc1766a6417a323d2445fbcab9539
naiiqili <naiqil@student.unimelb.edu.au>
2017-05-02 20:41:36 +0800

    update

--- recursive/train_bi_basic.sh
+++ recursive/train_bi_basic.sh
@@ -3,4 +3,4 @@ mkdir -p model/bi_basic
 
 # python train_bi_basic.py --fig_path='log/bi_basic/fig/' --bestmodel_dir='model/bi_basic/' --logdir='log/bi_basic/' --train_record='_data/train.record' --valid_record='_data/valid.record' --train_freq=1 --valid_freq=2 --valid_size=2
 
-CUDA_VISIBLE_DEVICES=0 python train_bi_basic.py --fig_path='log/bi_basic/fig/' --bestmodel_dir='model/bi_basic/' --logdir='log/bi_basic/' --train_record='tmp/train.record' --valid_record='tmp/valid.record' --train_freq=10 --valid_freq=200 --valid_size=500 --lr=0.001
+CUDA_VISIBLE_DEVICES=0 python train_bi_basic.py --fig_path='log/bi_basic/fig/' --bestmodel_dir='model/bi_basic/' --logdir='log/bi_basic/' --train_record='tmp/train.record' --valid_record='tmp/valid.record' --train_freq=10 --valid_freq=200 --valid_size=500 --lr=0.001 --patience=20
---
+++ recursive/train_bi_basic2.sh
@@ -0,0 +1,6 @@
+mkdir -p log/bi_basic2/fig
+mkdir -p model/bi_basic2
+
+python train_bi_basic2.py --fig_path='log/bi_basic2/fig/' --bestmodel_dir='model/bi_basic2/' --logdir='log/bi_basic2/' --train_record='_data/train.record' --valid_record='_data/valid.record' --train_freq=1 --valid_freq=2 --valid_size=2
+
+#CUDA_VISIBLE_DEVICES=0 python train_bi_basic2.py --fig_path='log/bi_basic2/fig/' --bestmodel_dir='model/bi_basic2/' --logdir='log/bi_basic2/' --train_record='tmp/train.record' --valid_record='tmp/valid.record' --train_freq=10 --valid_freq=200 --valid_size=500 --lr=0.001 --patience=20
