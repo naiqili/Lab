cd9c4d7549f9daf76cddd20cd36fe2325fc27a9e
naiiqili <naiqil@student.unimelb.edu.au>
2017-05-04 12:53:02 +0800

    update

--- recursive/train_bi_lstm.py
+++ recursive/train_bi_lstm.py
@@ -41,9 +41,9 @@ flags.DEFINE_boolean("load_model", False, "Whether load the best model")
 FLAGS = flags.FLAGS
 
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.FileHandler(FLAGS.logdir+FLAGS.target))
 logging.basicConfig(level = logging.DEBUG,
                     format = "%(asctime)s: %(name)s: %(levelname)s: %(message)s")
+logger.addHandler(logging.FileHandler(FLAGS.logdir+FLAGS.target))
 
 def report_figure(valid_history, train_history):
     (loss, acc, precision, recall, f1) = zip(*valid_history)
@@ -81,18 +81,36 @@ def report_figure(valid_history, train_history):
     except:
         pass
 
-def get_metrics(pred, target, is_leaf, only_leaf=True):
+def prop_op(pred, target, is_leaf, prop, left, right):
+    i = len(pred)-1
+    res = pred[:]
+    while i >= 0:
+        if not is_leaf[i]:
+            if prop == 'AND':
+                res[left[i]] = int(bool(res[left[i]]) and bool(res[i]))
+                res[right[i]] = int(bool(res[right[i]]) and bool(res[i]))
+            elif prop == 'OR':
+                res[left[i]] = int(bool(res[left[i]]) or bool(res[i]))
+                res[right[i]] = int(bool(res[right[i]]) or bool(res[i]))
+        i -= 1
+    return res
+
+def get_metrics(pred, target, is_leaf, only_leaf=True, prop=None, left=None, right=None):
+    if prop == None:
+        _pred = pred
+    else:
+        _pred = prop_op(pred, target, is_leaf, prop, left, right)
     v00, v01, v10, v11 = 0, 0, 0, 0
     for i in range(len(is_leaf)):
         if only_leaf and is_leaf[i] == 0:
             continue
-        if pred[i] == 0 and target[i] == 0:
+        if _pred[i] == 0 and target[i] == 0:
             v00 += 1
-        if pred[i] == 0 and target[i] == 1:
+        if _pred[i] == 0 and target[i] == 1:
             v01 += 1
-        if pred[i] == 1 and target[i] == 0:
+        if _pred[i] == 1 and target[i] == 0:
             v10 += 1
-        if pred[i] == 1 and target[i] == 1:
+        if _pred[i] == 1 and target[i] == 1:
             v11 += 1
     return v00, v01, v10, v11
 
@@ -162,26 +180,43 @@ def train():
             train_loss, train_summary, _ = sess.run([loss_ts, train_summary, train_op])
             train_history.append(train_loss)
             if _step % FLAGS.train_freq == 0:
-                logger.debug("Training loss: %f" % train_loss)
+                logger.debug("Step: %d Training: Loss: %f" % (_step, train_loss))
                 train_writer.add_summary(train_summary, _step)
 
             if _step != 0 and _step % FLAGS.valid_freq == 0:
+                logger.debug("Start validation")
                 valid_losses = []
                 metrics = 0, 0, 0, 0
+                a_metrics = 0, 0, 0, 0
+                o_metrics = 0, 0, 0, 0
                 for i in xrange(FLAGS.valid_size):
-                    valid_loss, valid_summary, valid_pred, target_v, is_leaf_v = \
-                        sess.run([valid_md.mean_loss, valid_md.loss_summary, valid_md.pred, target_vts, is_leaf_vts])
+                    valid_loss, valid_summary, valid_pred, target_v, is_leaf_v, left_v, right_v = \
+                        sess.run([valid_md.mean_loss, valid_md.loss_summary, valid_md.pred, target_vts, is_leaf_vts, left_vts, right_vts])
                     valid_losses.append(valid_loss)
-                    logger.debug("Validation loss %f" % valid_loss)
+                    #logger.debug("Validation loss %f" % valid_loss)
                     valid_writer.add_summary(valid_summary, _step)
                     new_metrics = get_metrics(valid_pred, target_v, is_leaf_v)
                     metrics = map(lambda (x,y): x+y, zip(metrics, new_metrics))
+                    a_new_metrics = get_metrics(valid_pred, target_v, is_leaf_v, prop='AND', left=left_v, right=right_v)
+                    a_metrics = map(lambda (x,y): x+y, zip(a_metrics, a_new_metrics))
+                    o_new_metrics = get_metrics(valid_pred, target_v, is_leaf_v, prop='OR', left=left_v, right=right_v)
+                    o_metrics = map(lambda (x,y): x+y, zip(o_metrics, o_new_metrics))
+                    
                 _00, _01, _10, _11 = metrics
+                a_00, a_01, a_10, a_11 = a_metrics
+                o_00, o_01, o_10, o_11 = o_metrics
+                print metrics
+                print a_metrics
+                print o_metrics
                 acc, precision, recall, f1 = collect_metrics(_00, _01, _10, _11)
+                a_acc, a_precision, a_recall, a_f1 = collect_metrics(a_00, a_01, a_10, a_11)
+                o_acc, o_precision, o_recall, o_f1 = collect_metrics(o_00, o_01, o_10, o_11)
                 mean_loss = np.mean(valid_losses)
-                logger.debug('Validation finish')
-                logger.debug('  mean loss: %f' % mean_loss)
-                logger.debug('  acc: %f, precision: %f, recall: %f, f1: %f' % (acc, precision, recall, f1))
+                logger.debug('Validation: finish')
+                logger.debug('Step: %d Validation: mean loss: %f' % (_step, mean_loss))
+                logger.debug('Validation: prop:None  acc: %f, precision: %f, recall: %f, f1: %f' % (acc, precision, recall, f1))
+                logger.debug('Validation: prop:AND  acc: %f, precision: %f, recall: %f, f1: %f' % (a_acc, a_precision, a_recall, a_f1))
+                logger.debug('Validation: prop:OR  acc: %f, precision: %f, recall: %f, f1: %f' % (o_acc, o_precision, o_recall, o_f1))
                 valid_history.append((mean_loss, acc, precision, recall, f1))
                 report_figure(valid_history, train_history)
                 logger.debug('Figure saved')
